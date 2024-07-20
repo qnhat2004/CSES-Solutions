fbf791a56b20bd5c29a78dc48f9d2c8a6391212b
quang nhat <nhatchien12@gmail.com>
2024-07-20 16:24:46 +0700

    add: Playlist.cpp

---
+++ Sorting and Searching/Playlist.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    long long a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }   
+    int l = 0, r = 0, res = 0;
+    unordered_set<long long> s;
+    while (r < n) {
+        while (s.find(a[r]) != s.end()) {
+            s.erase(a[l]);
+            l++;
+        }
+        s.insert(a[r]);
+        res = max(res, r - l + 1);
+        r++;
+    }
+    cout << res << endl;
+    return 0;
+}
